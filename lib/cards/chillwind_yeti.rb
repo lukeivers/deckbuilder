9de7b23803622119a06fe0db6c0a5c58bdcf5cda
Damen Knight <damen.knight@gmail.com>
2014-02-15 22:57:26 +0000

    fixed yeti

--- lib/cards/chillwind_yeti.rb
+++ lib/cards/chillwind_yeti.rb
@@ -2,11 +2,11 @@ require './cards/minion'
 
 class ChillwindYeti < Minion
   def initialize
-    super
     @cost = 4
     @attack = 4
     @max_health = 5
     @name = "Chillwind Yeti"
+    super
   end
 
 end
